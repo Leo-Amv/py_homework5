211592160aa2d5a500206d928833f7b9871ea439
Leo_Amv <leonidamvrosov@yandex.ru>
2022-09-21 23:31:13 +0500

    Easy AI added, gamemode choice added

--- task1.py
+++ task1.py
@@ -9,14 +9,35 @@
 from random import randint
 
 
-candies = 2021
-player = randint(1, 2)
-print('\nEnter the number of sweets from 1 to 28 !\n')
-print(f'\tPLAYER {player} STARTS!')
-print(f'\n\tTotal sweets:\t{candies}')
+def AI(candies, player):
+    if player == 1:
+        player = 'PLAYER'
+    else:
+        player = 'BOT'
+    print(f'\t{player} STARTS!')
+    while candies > 0:
+        if player == 'PLAYER':
+            n = int(
+                input(f'\n{player} turn :\t'))
+        else:
+            n = randint(1, 29)
+            print(f'\n{player} turn :\t{n}')
+        if 0 < n < 29:
+            candies -= n
+            print(f'Leftover candy:\t{candies}')
+            if candies <= 0:
+                print(f'\n\t{player} WINS!\n')
+            if player == 'PLAYER':
+                player = 'BOT'
+            else:
+                player = 'PLAYER'
+        else:
+            print('\n\tYou must enter the number from 1 to 28, try again !')
+            continue
 
 
-try:
+def PVP(candies, player):
+    print(f'\n\tPLAYER {player} STARTS!')
     while candies > 0:
         n = int(
             input(f'\nPlayer {player} turn :\t'))
@@ -32,5 +53,21 @@ try:
         else:
             print('\n\tYou must enter the number from 1 to 28, try again !')
             continue
+
+
+try:
+    candies = 200
+    player = randint(1, 2)
+    choise = int(input(
+        '\nEnter number of gamemode:\n\tPvP --> \t"1"\n\tEasy AI -->\t"2"\n\tSmart AI -->\t"3"\t'))
+    print('\nEnter the number of sweets from 1 to 28 !\n')
+    print(f'\tTotal sweets:\t{candies}')
+    if choise == 1:
+        PVP(candies, player)
+    elif choise == 2:
+        AI(candies, player)
+    else:
+        pass
+
 except ValueError:
     print('Incorrect data! You must enter number, try again!')
